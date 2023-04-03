b44d18058aa25de55a63517c84feaea42fe72a05
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-03 15:43:09 -0700

    Add files via upload

---
+++ payloadEx1/a.py
@@ -0,0 +1,19 @@
+end_hex=b"\x00\x00\x00\x00\x49\x45\x4e\x44\xae\x42\x60\x82"
+
+'''
+with open('image.png','ab') as f, open('code.py','rb') as e:
+    f.write(e.read())
+'''   
+    
+
+with open('image.png','rb') as f:
+    content=f.read()
+    offset=content.index(end_hex)
+    f.seek(offset+len(end_hex))
+    #print(f.read())
+    lines = f.readlines()
+    
+for line in lines:
+    print(line.decode())
+
+    
