05bc135e36b20f11e5a6ec20ef5198fa7d986080
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-03 15:19:14 -0700

    Create 2_1_HideFilesInPNG.py

---
+++ 2_1_HideFilesInPNG.py
@@ -0,0 +1,14 @@
+# Hide any File In PNG
+end_hex=b"\x00\x00\x00\x00\x49\x45\x4e\x44\xae\x42\x60\x82"
+
+with open('image.png','ab') as f, open('code.py','rb') as e:
+    f.write(e.read())
+    
+''' 
+with open('image.png','rb') as f:
+    content=f.read()
+    offset=content.index(end_hex)
+    f.seek(offset+len(end_hex))
+    print(f.read())
+'''
+    
