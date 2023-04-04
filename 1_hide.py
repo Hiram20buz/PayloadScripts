22957092ee2b1315d221e9ea06d2473b86eff753
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-03 17:00:28 -0700

    Create 1_hide.py

---
+++ 1_hide.py
@@ -0,0 +1,17 @@
+import numpy as np
+import PIL.Image
+
+message_to_hide="This is my secret message!"
+
+image=PIL.Image.open('image.png','r')
+width,height=image.size
+img_arr=np.array(list(image.getdata()))
+
+if image.mode == "P":
+    print("Not supported")
+    exit()
+    
+channels= 4 if image.mode == "RGBA" else 3
+
+pixels=img_arr.size // channels
+
